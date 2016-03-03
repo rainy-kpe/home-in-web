4c4eac1cd5c8f930c7ec9e007d2532223b40e9aa
Kimmo Pekkola <rainy@iki.fi>
2016-03-03 18:28:28 +0200

    Refactored rss feed from the list

---
+++ frontend/components/rf-list/rf-list.ts
@@ -0,0 +1,160 @@
+/// <reference path="../../../typings/browser.d.ts" />
+"use strict";
+
+declare var sha256: any;
+
+/**
+ * List web component
+ *
+ * Shows the feed contents as a list
+ */
+@component("rf-list")
+class RFList extends polymer.Base {
+
+    // The feed item
+    @property({type: Object})
+    feed: Feed;
+
+    // All the feed entries
+    @property({type: Array})
+    entries: FeedItem[];
+
+    // The firebase authentication data
+    @property({type: Object, notify: true})
+    auth: FirebaseAuthData;
+
+    // Flag to show only the starred items
+    @property({type: Boolean, value: false})
+    showStarred: boolean;
+
+    // The firebase instance
+    _firebase: Firebase = new Firebase('https://rainfeeds-dev.firebaseio.com/feeds');
+
+    // The full feed entries
+    _normalEntries: any;
+
+    // The starred entries
+    _starredEntries: any;
+
+    // Callback to listen for star changes from the database
+    _starCallback: any;
+
+    /**
+     * Handler for the star-changed event.
+     * Writes the starred entry to the database or removes it from there.
+     * @param  {any} e The event
+     * @returns void
+     */
+    _starEventHandler: any = (e: any): void => {
+        const key: string = sha256(e.detail.link);
+
+        console.log(`Changing the star value for "${e.detail.title}"`);
+
+        if (e.detail.starred) {
+            this._firebase.child(this.auth.uid).child(this.feed.id).child("starred").child(key)
+                .set(e.detail, (error: any) => {
+                if (error) {
+                    console.log('Writing the starred entry failed: ' + JSON.stringify(error));
+                }
+            });
+        } else {
+            this._firebase.child(this.auth.uid).child(this.feed.id).child("starred").child(key).remove((error: any) => {
+                if (error) {
+                    console.log('Removing the starred entry failed: ' + JSON.stringify(error));
+                }
+            });
+        }
+    };
+
+    /**
+     * Adds stars to the entries which were found from the database
+     * @returns void
+     */
+    _starEntries(): void {
+        if (this._starredEntries && this.entries) {
+            this.entries.forEach((entry: any, index: number) => {
+                const key: string = sha256(entry.link);
+                if (this._starredEntries[key]) {
+                    this.set("entries." + index + ".starred", true);
+                } else if (entry.starred) {
+                    this.set("entries." + index + ".starred", false);
+                }
+            });
+        }
+    }
+
+    /**
+     * Observer for the "showStarred" property. Changes the entries between the normal feed and starred items.
+     * @param  {boolean} newVal The new value
+     * @param  {boolean} oldVal The old value
+     * @returns void
+     */
+    @observe("showStarred")
+    _showStarredChanged(newVal: boolean, oldVal: boolean): void {
+        if (newVal) {
+            this._normalEntries = this.entries;
+            this.set("entries", _.values(this._starredEntries).sort((a: FeedItem, b: FeedItem) => {
+                            const dtA: Date = new Date(a.date);
+                            const dtB: Date = new Date(b.date);
+                            return dtB.getTime() - dtA.getTime();
+                        }));
+        } else {
+            this.set("entries", this._normalEntries);
+        }
+    }
+
+    /**
+     * Observer for the "entries" property. Adds click handler for the star button in the item.
+     * @param  {any[]} newVal The new values
+     * @param  {any[]} oldVal The old values
+     * @returns void
+     */
+    @observe("entries")
+    _entriesChanged(newVal: any[], oldVal: any[]): void {
+        // Set the stars to the entries
+        this._starEntries();
+
+        this.async(() => {
+            // Add handlers for the "star-changed" events from the items
+            const items: any[] = <any> Polymer.dom(this.root).querySelectorAll(".item");
+            items.map((item: any) => {
+                item.removeEventListener("star-changed", this._starEventHandler);
+                item.addEventListener("star-changed", this._starEventHandler);
+            });
+        });
+    }
+
+    /**
+     * Registers the handlers
+     * @returns void
+     */
+    attached(): void {
+
+        // When authentication changes read all the starred entries from the database
+        this._firebase.onAuth((authData: FirebaseAuthData) => {
+            if (authData && !this._starredEntries) {
+                this._starCallback = this._firebase.child(authData.uid).child(this.feed.id).child("starred")
+                    .on("value", (snapshot: FirebaseDataSnapshot) => {
+                    this._starredEntries = snapshot.val();
+                    this._starEntries();
+
+                }, (error: any) => {
+                    console.log("Reading the starred entries failed: " + error.code);
+                });
+            } else if (!authData && this.auth) {
+                // On logout clear all cached values
+                this._firebase.child(this.auth.uid).child(this.feed.id).child("starred").off("value", this._starCallback);
+                this._starredEntries = undefined;
+                this._starEntries();
+
+                // Switch to the normal mode if we're showning starred items
+                if (this.showStarred) {
+                    this.set("showStarred", false);
+                }
+            }
+            this.set("auth", authData);
+        });
+    }
+}
+
+RFList.register();
--- frontend/components/rf-rss-feed/rf-rss-feed.ts
+++ frontend/components/rf-rss-feed/rf-rss-feed.ts
@@ -1,87 +1,21 @@
 /// <reference path="../../../typings/browser.d.ts" />
 "use strict";
 
-declare var sha256: any;
-
 /**
  * Rss Feed web component
  *
- * Reads the content for the feed
+ * Downloads the rss feeds for the urls
  */
 @component("rf-rss-feed")
 class RFRssFeed extends polymer.Base {
 
     // The feed item
     @property({type: Object})
-    feed: any;
+    feed: Feed;
 
     // All the feed entries
-    @property({type: Array})
-    entries: any[];
-
-    // The firebase authentication data
-    @property({type: Object, notify: true})
-    auth: FirebaseAuthData;
-
-    // Flag to show only the starred items
-    @property({type: Boolean, value: false})
-    showStarred: boolean;
-
-    // The firebase instance
-    _firebase: Firebase = new Firebase('https://rainfeeds-dev.firebaseio.com/feeds');
-
-    // The full feed entries
-    _normalEntries: any;
-
-    // The starred entries
-    _starredEntries: any;
-
-    // Callback to listen for star changes from the database
-    _starCallback: any;
-
-    /**
-     * Handler for the star-changed event.
-     * Writes the starred entry to the database or removes it from there.
-     * @param  {any} e The event
-     * @returns void
-     */
-    _starEventHandler: any = (e: any): void => {
-        const key: string = sha256(e.detail.link);
-
-        console.log(`Changing the star value for "${e.detail.title}"`);
-
-        if (e.detail.starred) {
-            this._firebase.child(this.auth.uid).child(this.feed.id).child("starred").child(key)
-                .set(e.detail, (error: any) => {
-                if (error) {
-                    console.log('Writing the starred entry failed: ' + JSON.stringify(error));
-                }
-            });
-        } else {
-            this._firebase.child(this.auth.uid).child(this.feed.id).child("starred").child(key).remove((error: any) => {
-                if (error) {
-                    console.log('Removing the starred entry failed: ' + JSON.stringify(error));
-                }
-            });
-        }
-    };
-
-    /**
-     * Adds stars to the entries which were found from the database
-     * @returns void
-     */
-    _starEntries(): void {
-        if (this._starredEntries && this.entries) {
-            this.entries.forEach((entry: any, index: number) => {
-                const key: string = sha256(entry.link);
-                if (this._starredEntries[key]) {
-                    this.set("entries." + index + ".starred", true);
-                } else if (entry.starred) {
-                    this.set("entries." + index + ".starred", false);
-                }
-            });
-        }
-    }
+    @property({type: Array, notify: true})
+    entries: FeedItem[];
 
     /**
      * Converts the rss feed entry to FeedItem
@@ -110,76 +44,10 @@ class RFRssFeed extends polymer.Base {
     }
 
     /**
-     * Observer for the "showStarred" property. Changes the entries between the normal feed and starred items.
-     * @param  {boolean} newVal The new value
-     * @param  {boolean} oldVal The old value
-     * @returns void
-     */
-    @observe("showStarred")
-    _showStarredChanged(newVal: boolean, oldVal: boolean): void {
-        if (newVal) {
-            this._normalEntries = this.entries;
-            this.set("entries", _.values(this._starredEntries).sort((a: any, b: any) => {
-                            const dtA: Date = new Date(a.publishedDate);
-                            const dtB: Date = new Date(b.publishedDate);
-                            return dtB.getTime() - dtA.getTime();
-                        }));
-        } else {
-            this.set("entries", this._normalEntries);
-        }
-    }
-
-    /**
-     * Observer for the "entries" property. Adds click handler for the star button in the item.
-     * @param  {any[]} newVal The new values
-     * @param  {any[]} oldVal The old values
-     * @returns void
-     */
-    @observe("entries")
-    _entriesChanged(newVal: any[], oldVal: any[]): void {
-        // Set the stars to the entries
-        this._starEntries();
-
-        this.async(() => {
-            // Add handlers for the "star-changed" events from the items
-            const items: any[] = <any> Polymer.dom(this.root).querySelectorAll(".item");
-            items.map((item: any) => {
-                item.removeEventListener("star-changed", this._starEventHandler);
-                item.addEventListener("star-changed", this._starEventHandler);
-            });
-        });
-    }
-
-    /**
      * Registers the handlers
      * @returns void
      */
     attached(): void {
-
-        // When authentication changes read all the starred entries from the database
-        this._firebase.onAuth((authData: FirebaseAuthData) => {
-            if (authData && !this._starredEntries) {
-                this._starCallback = this._firebase.child(authData.uid).child(this.feed.id).child("starred")
-                    .on("value", (snapshot: FirebaseDataSnapshot) => {
-                    this._starredEntries = snapshot.val();
-                    this._starEntries();
-
-                }, (error: any) => {
-                    console.log("Reading the starred entries failed: " + error.code);
-                });
-            } else if (!authData && this.auth) {
-                // On logout clear all cached values
-                this._firebase.child(this.auth.uid).child(this.feed.id).child("starred").off("value", this._starCallback);
-                this._starredEntries = undefined;
-                this._starEntries();
-
-                // Switch to the normal mode if we're showning starred items
-                if (this.showStarred) {
-                    this.set("showStarred", false);
-                }
-            }
-            this.set("auth", authData);
-        });
 
         this.async(() => {
             // Add error handler for all the feeds
@@ -192,16 +60,17 @@ class RFRssFeed extends polymer.Base {
             feeds.map((feed: any) => feed.addEventListener("google-feeds-response", (e: any) => {
 
                 if (this.feed.urls.length === 1) {
-                    this.set("entries", feed.results.entries);
+                    this.set("entries", feed.results.entries.map((entry: any) => this._createItem(entry)));
 
                     // Update feed every 15 minutes
                     this.async(() => feed._fetchFeeds(), 15 * 60 * 1000);
                 } else {
                     // Merge feeds
-                    const all: any[] = _.uniqBy((this.entries || []).concat(feed.results.entries), (v: any) => v.title)
-                        .sort((a: any, b: any) => {
-                            const dtA: Date = new Date(a.publishedDate);
-                            const dtB: Date = new Date(b.publishedDate);
+                    const newItems: FeedItem[] = feed.results.entries.map((entry: any) => this._createItem(entry));
+                    const all: any[] = _.uniqBy((this.entries || []).concat(newItems), (v: any) => v.title)
+                        .sort((a: FeedItem, b: FeedItem) => {
+                            const dtA: Date = new Date(a.date);
+                            const dtB: Date = new Date(b.date);
                             return dtB.getTime() - dtA.getTime();
                         });
 
